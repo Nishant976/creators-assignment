c0130fe6c29505f9f182e70cba8cdd1f32def4a6
Nishant Singh <2105976@kiit.ac.in>
2023-09-25 17:34:02 +0530

    Initial Commit

---
+++ accounts/views.py
@@ -0,0 +1,17 @@
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from . import forms
+from .models import Image
+# Create your views here.
+
+class SignUp(CreateView):
+    form_class = forms.UserCreateForm
+    success_url = reverse_lazy("login")
+    template_name = "accounts/signup.html"
+
+
+class CreatePostView(CreateView):
+   template_name="accounts/post.html"
+   model=Image
+   fields="__all__"
+   success_url=reverse_lazy("login")
